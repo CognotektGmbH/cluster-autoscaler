8c7fda81abaf683f19a5ed31d30979f12f403c27
vanappmi <vanappeldorn@gmail.com>
2021-03-09 14:43:49 +0100

    Update irsa.tf

--- irsa.tf
+++ irsa.tf
@@ -1,6 +1,6 @@
 module "iam_assumable_role_cluster_autoscaler" {
   source       = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version      = "~> v2.14.0"
+  version      = "~> v3.12.0"
   create_role  = true
   role_name    = "${local.base_name}-${random_string.lower.result}"
   provider_url = replace(var.eks_cluster_oidc_issuer_url, "https://", "")
