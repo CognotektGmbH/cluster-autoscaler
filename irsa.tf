d152b3989998fbd0fbd84b65535a05ac615ae093
Michael van Appeldorn <michael.vanappeldorn.extern@consilica.de>
2022-01-04 13:03:34 +0100

    update version

--- irsa.tf
+++ irsa.tf
@@ -1,6 +1,6 @@
 module "iam_assumable_role_cluster_autoscaler" {
   source       = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version      = "~> v3.12.0"
+  version      = "4.8.0"
   create_role  = true
   role_name    = "${local.base_name}-${random_string.lower.result}"
   provider_url = replace(var.eks_cluster_oidc_issuer_url, "https://", "")
--- service-account.tf
+++ service-account.tf
@@ -3,7 +3,7 @@ resource "kubernetes_service_account" "cluster_autoscaler" {
     name      = local.cluster_autoscaler_service_account_name
     namespace = var.namespace
     annotations = {
-      "eks.amazonaws.com/role-arn" = module.iam_assumable_role_cluster_autoscaler.this_iam_role_arn
+      "eks.amazonaws.com/role-arn" = module.iam_assumable_role_cluster_autoscaler.iam_role_arn
     }
     labels = local.tags
   }
